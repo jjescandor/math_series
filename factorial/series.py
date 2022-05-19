58eeb69961ba1ceed33fed2e83f48f6b628b77d0
JJ Escandor <jj.p.escandor@gmail.com>
2022-05-19 01:10:17 -0400

    Added more tests

--- factorial/series.py
+++ factorial/series.py
@@ -4,9 +4,10 @@ def fibonacci(n):
     """
     This is a recursive function that starts with 0 and 1 and adds the previous two
     numbers to get the next number.
+    It returns the nth number in the Fibonacci sequence
     """
     if n < 0:
-        print('incorrect input')
+        return 'incorrect input'
     elif n == 0:
         return 0
     elif n == 1 or n == 2:
@@ -19,6 +20,7 @@ def lucas(n):
     """
     This is a recursive function that starts with 2 and 1 and adds the previous two
     numbers to get the next number.
+    It returns the nth number in the Lucas sequence
     """
     if n < 0:
         return 'incorrect input'
@@ -34,6 +36,7 @@ def sum_series(n, num_zero=0, num_one=1):
     """
     This is a recursive function that that starts with and two numbers
     specified by the user and adds the previous two numbers to get the next number.
+    It returns the nth number in the Fibonacci, Lucas or other kind of sequence
     """
     if n < 0:
         return 'incorrect input'
